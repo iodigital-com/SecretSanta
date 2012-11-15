b10f9abc82993f5a50d6ea461b4685bc7323571f
Tom Van Looy <tom@ctors.net>
2012-11-15 19:05:14 +0100

    deploy target

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'railsless-deploy'
 
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
+set :application, "SecretSanta"
+set :repository,  "git://github.com/tvlooy/SecretSanta.git"
+set :deploy_to, "/var/www/secretsanta.ctors.net/"
+set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :keep_releases, 3
+after "deploy", "deploy:cleanup"
+
+server "secretsanta.ctors.net:9999", :web, :app, :db, :primary => true
+#role :web, "your web-server here"                          # Your HTTP server, Apache/etc
+#role :app, "your app-server here"                          # This may be the same as your `Web` server
+#role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
