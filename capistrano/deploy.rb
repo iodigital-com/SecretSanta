a737704576a1c991e6b104946f1636f3f10f827f
Tom Van Looy <tom@ctors.net>
2012-11-20 19:19:35 +0100

    new deploy target

--- capistrano/deploy.rb
+++ capistrano/deploy.rb
@@ -2,11 +2,13 @@ require 'railsless-deploy'
 
 set :application, "SecretSanta"
 set :repository,  "git://github.com/tvlooy/SecretSanta.git"
-set :deploy_to, "/var/www/secretsanta.ctors.net/"
+set :deploy_to, "/var/www/secretsanta/"
 set :scm, :git
 set :deploy_via, :rsync_with_remote_cache
 
-set :keep_releases, 3
+set :keep_releases, 2
 after "deploy", "deploy:cleanup"
 
-server "secretsanta.ctors.net:9999", :web, :app, :db, :primary => true
+set :user, "secretsanta"
+set :use_sudo, false
+server "192.168.1.216", :web, :app, :db, :primary => true # labs server
