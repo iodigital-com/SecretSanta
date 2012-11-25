0ac1e29b984fee8aa2ac60d383e8ff7ab2c66157
Jimmy Henderickx <jimmyhdx@gmail.com>
2012-11-25 20:08:26 +0100

    Entry view page added

---
+++ htdocs/src/Intracto/SecretSantaBundle/Controller/EntryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Intracto\SecretSantaBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Intracto\SecretSantaBundle\Entity\Entry;
+
+class EntryController extends Controller
+{
+    /**
+     * @Route("/entry/{url}", name="entry_view")
+     * @Template()
+     */
+    public function indexAction($url)
+    {
+        $this->getEntry($url);
+
+        return array(
+            'entry' => $this->entry,
+        );
+    }
+
+    /**
+     * Retrieve entry by url
+     *
+     * @param string $url
+     *
+     * @return boolean
+     */
+    protected function getEntry($url)
+    {
+        $this->entry = $this->getDoctrine()->getRepository('IntractoSecretSantaBundle:Entry')->findOneByUrl($url);
+
+        if (!is_object($this->entry)) {
+            throw new \Symfony\Component\HttpKernel\Exception\NotFoundHttpException();
+        }
+
+        return true;
+    }
+}
