f5e8feb26ac404ddf98fcee6b09eb8343137a496
Tom Van Looy <tom@ctors.net>
2012-11-30 10:21:45 +0100

    cap deploy instructions (todo's)

--- capistrano/deploy.rb
+++ capistrano/deploy.rb
@@ -14,3 +14,14 @@ after "deploy", "deploy:cleanup"
 set :user, "secretsanta"
 set :use_sudo, false
 server "192.168.1.216", :web, :app, :db, :primary => true # labs server
+
+# Usage (yes, this is todo):
+# 1) chown -R secretsanta:www-data * 
+# 2) cap deploy
+# 3) cp -R shared/vendor/ current/htdocs/
+#    cp shared/parameters.yml current/htdocs/app/config/
+#    cd current/htdocs/
+#    /var/www/secretsanta/shared/composer.phar update
+#    app/console cache:clear -env=prod
+#    chown -R secretsanta:www-data *
+#    chmod -R 777 app/cache/ app/logs/
