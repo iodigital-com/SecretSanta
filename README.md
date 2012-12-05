60cd3de6b2647d3bb6b89f523732ae1c786549e9
Tom Van Looy <tom@ctors.net>
2012-12-05 21:04:35 +0100

    roundcube improvements, fix #8

--- README.md
+++ README.md
@@ -35,5 +35,3 @@ Note that it is sometimes necessary to manually restart apache (so it picks up i
 ###Mailing
 
 All mail that is sent on the system, from any address to any address is delivered to the vagrant user. You can read this user's mailbox with the webmail that is configured on http://33.33.33.10/roundcube (user: vagrant, pass: vagrant)
-
-If you can't login to roundcube, you probably didn't send any mails yet (so the user's mail directory doesn't exist yet). This is listed as a bug, see [#8](https://github.com/tvlooy/SecretSanta/issues/8).
--- puppet/modules/postfix/files/webmail.sh
+++ puppet/modules/postfix/files/webmail.sh
@@ -7,6 +7,10 @@ sed -i 's/#    Alias \/roundcube/Alias \/roundcube/' /etc/apache2/conf.d/roundcu
 sed -i "s/'default_host'] = ''/'default_host'] = 'localhost'/" /etc/roundcube/main.inc.php 
 sed -i "s/'from', 'date'/'from', 'to', 'date'/" /etc/roundcube/main.inc.php 
 sed -i "s/'show_images'] = 0/'show_images'] = 2/" /etc/roundcube/main.inc.php
+sed -i "s/'create_default_folders'] = FALSE/'create_default_folders'] = TRUE/" /etc/roundcube/main.inc.php
+
+# create empty mailbox
+mkdir -p /home/vagrant/Maildir/{cur,new,tmp}
 
 # webserver runs as vagrant instead of www-data
 chgrp vagrant /etc/roundcube/main.inc.php 
