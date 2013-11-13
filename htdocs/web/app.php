b392fa0648c8848ec249ed621469d23f162e30e6
Tom Van Looy <tom@ctors.net>
2013-11-13 23:00:01 +0100

    use APC for autoloading fix #26

--- htdocs/web/app.php
+++ htdocs/web/app.php
@@ -7,10 +7,8 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
 // Use APC for autoloading to improve performance
 // Change 'sf2' by the prefix you want in order to prevent key conflict with another application
-/*
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
-*/
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
