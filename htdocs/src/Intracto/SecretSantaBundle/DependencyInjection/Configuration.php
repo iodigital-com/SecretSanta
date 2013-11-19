ca925bdf1689cd446be8fecb60feaf09966cbef2
Tom Van Looy <tom@ctors.net>
2013-11-19 21:38:17 +0100

    Zap unused parameters

--- htdocs/src/Intracto/SecretSantaBundle/DependencyInjection/Configuration.php
+++ htdocs/src/Intracto/SecretSantaBundle/DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('intracto_secret_santa');
+        $treeBuilder->root('intracto_secret_santa');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
