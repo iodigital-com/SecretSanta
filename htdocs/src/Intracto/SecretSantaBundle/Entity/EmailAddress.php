f73ab4b9e21e1260992eef66457796a8eea7331d
Tom Van Looy <tom@ctors.net>
2013-11-25 20:56:04 +0100

    Allow email address changes

---
+++ htdocs/src/Intracto/SecretSantaBundle/Entity/EmailAddress.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Intracto\SecretSantaBundle\Entity;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class EmailAddress
+{
+    /**
+     * @var string $emailAddress
+     *
+     * @Assert\NotBlank()
+     * @Assert\Email(
+     *     message = "The email '{{ value }}' is not a valid email.",
+     *     checkMX = true
+     * )
+     */
+    private $emailAddress;
+
+    public function __construct($emailAddress)
+    {
+        $this->emailAddress = $emailAddress;
+    }
+
+    public function __toString()
+    {
+        return $this->emailAddress;
+    }
+}
