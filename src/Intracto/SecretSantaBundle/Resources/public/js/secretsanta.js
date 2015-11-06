02a2986087969a4730694efb0caa579b646853de
Tom Van Looy <tom@ctors.net>
2015-11-07 00:43:38 +0100

    Style

--- src/Intracto/SecretSantaBundle/Resources/public/js/secretsanta.js
+++ src/Intracto/SecretSantaBundle/Resources/public/js/secretsanta.js
@@ -1,14 +1,14 @@
-$("#header").click(function(){
-    window.location=$(this).find("a#homelink").attr("href");
+$("#header").click(function() {
+    window.location = $(this).find("a#homelink").attr("href");
     return false;
 });
 
 
-$(document).ready(function(){
-  $('.lang__selection select').on('change', changeLanguage);
+$(document).ready(function() {
+    $('.lang__selection select').on('change', changeLanguage);
 });
 
 
 function changeLanguage(e) {
-  window.location = $(this).val();
+    window.location = $(this).val();
 }
