f1ae7c26139c4ee3ab9cbaf16cdc33b3182284d3
Tom Van Looy <tom@ctors.net>
2015-11-07 01:07:18 +0100

    Fix: language switcher broken

--- src/Intracto/SecretSantaBundle/Resources/public/js/secretsanta.js
+++ src/Intracto/SecretSantaBundle/Resources/public/js/secretsanta.js
@@ -1,13 +1,11 @@
-$("#header").click(function() {
-    window.location = $(this).find("a#homelink").attr("href");
-    return false;
-});
-
+//$("#header").click(function() {
+//    window.location = $(this).find("a#homelink").attr("href");
+//    return false;
+//});
 
 $(document).ready(function() {
     $('.lang__selection select').on('change', changeLanguage);
 });
-
 
 function changeLanguage(e) {
     window.location = $(this).val();
