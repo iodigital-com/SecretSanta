8c682a4c7f3cc3dfaf196b1508d23fb47966dd70
Tom Van Looy <tom@ctors.net>
2016-02-22 11:09:50 +0100

    Switch apc to apcu

--- shell_provisioner/module/php.sh
+++ shell_provisioner/module/php.sh
@@ -2,7 +2,7 @@
 
 # PHP
 
-apt-get -y install php5 php5-intl php5-xdebug php-apc
+apt-get -y install php5 php5-intl php5-xdebug php5-apcu
 
 # PHP config
 sed -i "s#;date.timezone =#date.timezone = Europe/Brussels#" /etc/php5/apache2/php.ini
