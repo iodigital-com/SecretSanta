83b448ad478b57b62b7837589243f9de1eaa0794
NIELS ROELS <niels.roels@student.thomasmore.be>
2016-02-24 09:43:24 +0100

    Removed comments from Reportingservice

--- src/Intracto/SecretSantaBundle/Service/Report/ReportingService.php
+++ src/Intracto/SecretSantaBundle/Service/Report/ReportingService.php
@@ -17,56 +17,52 @@ class ReportingService
 
     public function getPools(Request $request)
     {
-        /*if ($this->cache->contains('tracked_pools')) {
-            return unserialize($this->cache->fetch('tracked_pools'));
-        }*/
         $dbal = $this->doctrine->getConnection();
         $year = $request->get('year');
+
         if ($year == "all" || $year == '') {
             $pools = $dbal->fetchAll('SELECT count(*) as poolCount FROM Pool');
         } else {
             $pools = $dbal->fetchAll('SELECT count(*) as poolCount FROM Pool WHERE year(sentdate) = ' . $year);
-            //$this->cache->save('track_pools', serialize($pools), 3600);
         }
+
         return $pools;
     }
 
     public function getEntries(Request $request)
     {
-        /*if ($this->cache->contains('tracked_entries')) {
-            return unserialize($this->cache->fetch('tracked_entries'));
-        }*/
         $dbal = $this->doctrine->getConnection();
         $year = $request->get('year');
+
         if ($year == "all" || $year == '') {
             $entries = $dbal->fetchAll('SELECT count(*) as entryCount FROM Entry JOIN Pool on Pool.id = Entry.poolId');
         } else {
             $entries = $dbal->fetchAll('SELECT count(*) as entryCount FROM Entry JOIN Pool on Pool.id = Entry.poolId where year(Pool.sentdate) = ' . $year);
-            //$this->cache->save('tracked_entries', serialize($entries), 3600);
         }
+
         return $entries;
     }
 
     public function getFinishedWishlists(Request $request)
     {
-        /*if ($this->cache->contains('tracked_wishlists')) {
-            return unserialize($this->cache->fetch('tracked_wishlists'));
-        }*/
         $dbal = $this->doctrine->getConnection();
         $year = $request->get('year');
+
         if ($year == "all" || $year == '') {
             $wishlists = $dbal->fetchAll('SELECT count(*) as wishListCount FROM Entry JOIN Pool on Pool.id = Entry.poolId WHERE wishlist_updated = TRUE');
         } else {
             $wishlists = $dbal->fetchAll('SELECT count(*) as wishListCount FROM Entry JOIN Pool on Pool.id = Entry.poolId where year(Pool.sentdate) = ' . $year . ' AND Entry.wishlist_updated = TRUE');
-            //$this->cache->save('tracked_wishlists', serialize($wishlists), 3600);
         }
+
         return $wishlists;
     }
 
     public function getYears()
     {
         $dbal = $this->doctrine->getConnection();
+
         $featured_years = $dbal->fetchAll('SELECT DISTINCT year(sentdate) as featured_year FROM Pool WHERE year(sentdate) IS NOT NULL ORDER BY year(sentdate) DESC');
+
         return $featured_years;
     }
 }
