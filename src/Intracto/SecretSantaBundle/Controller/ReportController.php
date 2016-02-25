78f4265dab3a53b66eef0fca6c39ecd0578903b8
NIELS ROELS <niels.roels@student.thomasmore.be>
2016-02-25 13:49:43 +0100

    Removed too many use statements

--- src/Intracto/SecretSantaBundle/Controller/ReportController.php
+++ src/Intracto/SecretSantaBundle/Controller/ReportController.php
@@ -3,6 +3,8 @@
 namespace Intracto\SecretSantaBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
 class ReportController extends Controller
