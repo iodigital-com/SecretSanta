f668ca6e88511f0c8628f4e147d36985da1a363c
Alexandre Tozzi <alexandre.tozzi@student.thomasmore.be>
2016-02-29 12:25:06 +0100

    jquery after and javascript fix

--- src/Intracto/SecretSantaBundle/Resources/public/js/pool.create.js
+++ src/Intracto/SecretSantaBundle/Resources/public/js/pool.create.js
@@ -3,7 +3,7 @@ function addNewEntry(collectionHolder) {
     var prototype = collectionHolder.attr('data-prototype');
     // Adjust entry prototype for correct naming
     var number_of_entries = collectionHolder.children().length; // Note, owner is not counted as entry
-    var newFormHtml = prototype.replace(/__name__/g, number_of_entries).replace(/__entrycount__/g, number_of_entries + 1);
+    var newFormHtml = prototype.replace(/__name__/g, number_of_entries).replace(/__entrycount__/g, number_of_entries);
 
     // Add new entry to pool with animation
     var newForm = $(newFormHtml);
@@ -19,16 +19,16 @@ function addNewEntry(collectionHolder) {
 
 function bindDeleteButtonEvents() {
     // Loop over all delete buttons
-    $('button.remove-entry').each(function(i) {
+    $('button.remove-entry').each(function (i) {
         // Remove any previously binded event
         $(this).off('click');
 
         // Bind event
-        $(this).click(function(e) {
+        $(this).click(function (e) {
 
             e.preventDefault();
 
-            $('table tr.entry.not-owner:gt(' + i + ')').each(function(j) {
+            $('table tr.entry.not-owner:gt(' + i + ')').each(function (j) {
                 // Move values from next row to current row
                 var next_row_name = $('table tr.entry.not-owner:eq(' + (i + j + 1) + ') input.entry-name').val();
                 var next_row_mail = $('table tr.entry.not-owner:eq(' + (i + j + 1) + ') input.entry-mail').val();
@@ -54,22 +54,22 @@ function bindDeleteButtonEvents() {
 var collectionHolder = $('table.entries tbody');
 
 /* Document Ready */
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
     //Add eventlistener on add-new-entry button
-    $('.add-new-entry').click(function(e) {
+    $('.add-new-entry').click(function (e) {
         e.preventDefault();
         addNewEntry(collectionHolder);
     });
 
     // If form has more then 3 entries, provide delete functionality
-    if($('table tr.entry').length > 3){
+    if ($('table tr.entry').length > 3) {
         bindDeleteButtonEvents();
         $('.remove-entry').removeClass('disabled');
     }
 
     // Add smooth scroll
-    $('a.btn-started').click(function() {
+    $('a.btn-started').click(function () {
         $.smoothScroll({
             scrollTarget: '#mysanta'
         });
