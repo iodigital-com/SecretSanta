22e0a0d4dc8ba9d2a84fc1b2ec2941a1c9d4348e
Alexandre Tozzi <alexandre.tozzi@student.thomasmore.be>
2016-03-02 15:51:23 +0100

    google analytics works now

--- src/Intracto/SecretSantaBundle/Controller/ReportController.php
+++ src/Intracto/SecretSantaBundle/Controller/ReportController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Google_Client;
+use Google_Service_Analytics;
 
 class ReportController extends Controller
 {
