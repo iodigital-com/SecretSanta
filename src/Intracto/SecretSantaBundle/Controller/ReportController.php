4c2387af60076181fbf9872a51d179f65bbc8af8
NIELS ROELS <niels.roels@student.thomasmore.be>
2016-03-03 09:36:17 +0100

    Added Google Analytics data into query file | made service | added charts to report page

--- src/Intracto/SecretSantaBundle/Controller/ReportController.php
+++ src/Intracto/SecretSantaBundle/Controller/ReportController.php
@@ -6,8 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
-use Google_Client;
-use Google_Service_Analytics;
 
 class ReportController extends Controller
 {
@@ -17,24 +15,29 @@ class ReportController extends Controller
      */
     public function reportAction(Request $request)
     {
-        $report = $this->get('intracto_secret_santa.report');
+        $reportQuery = $this->get('intracto_secret_santa.reporting');
+        $analyticsQuery = $this->get('intracto_secret_santa.analytics');
         $currentYear = $request->get('year', 'all');
 
         try {
             if ($currentYear != 'all') {
-                $dataPool = $report->getPoolReport($currentYear);
+                $dataPool = $reportQuery->getPoolReport($currentYear);
+                $googleDataPool = $analyticsQuery->getAnalyticsReport($currentYear);
             } else {
-                $dataPool = $report->getPoolReport();
+                $dataPool = $reportQuery->getPoolReport();
+                $googleDataPool = $analyticsQuery->getFullAnalyticsReport();
             }
         } catch (\Exception $e) {
             $currentYear = [];
             $dataPool = [];
+            $googleDataPool = [];
         }
 
         return [
             'current_year' => $currentYear,
             'data_pool' => $dataPool,
             'featured_years' => $this->get('intracto_secret_santa.featured_years')->getFeaturedYears(),
+            'google_data_pool' => $googleDataPool,
         ];
     }
 }
