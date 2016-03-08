254cf43aa5741fc21f54e193af5be846c6684747
NIELS ROELS <niels.roels@student.thomasmore.be>
2016-03-08 16:26:02 +0100

    Catch error if second season returns no data

--- src/Intracto/SecretSantaBundle/Query/EntryReportQuery.php
+++ src/Intracto/SecretSantaBundle/Query/EntryReportQuery.php
@@ -250,7 +250,12 @@ class EntryReportQuery
     public function calculateEntryCountDifferenceBetweenSeasons(Season $season1, Season $season2)
     {
         $entryCountSeason1 = $this->countEntries($season1);
-        $entryCountSeason2 = $this->countEntries($season2);
+        try {
+            $entryCountSeason2 = $this->countEntries($season2);
+        } catch(\Exception $e) {
+            return $entryCountSeason1[0]['entryCount'];
+        }
+
 
         return $entryCountSeason1[0]['entryCount'] - $entryCountSeason2[0]['entryCount'];
     }
@@ -263,7 +268,11 @@ class EntryReportQuery
     public function calculateConfirmedEntryCountDifferenceBetweenSeasons(Season $season1, Season $season2)
     {
         $confirmedEntryCountSeason1 = $this->countConfirmedEntries($season1);
-        $confirmedEntryCountSeason2 = $this->countConfirmedEntries($season2);
+        try {
+            $confirmedEntryCountSeason2 = $this->countConfirmedEntries($season2);
+        } catch(\Exception $e) {
+            return $confirmedEntryCountSeason1[0]['confirmedEntryCount'];
+        }
 
         return $confirmedEntryCountSeason1[0]['confirmedEntryCount'] - $confirmedEntryCountSeason2[0]['confirmedEntryCount'];
     }
@@ -276,7 +285,12 @@ class EntryReportQuery
     public function calculateDistinctEntryCountDifferenceBetweenSeasons(Season $season1, Season $season2)
     {
         $distinctEntryCountSeason1 = $this->countDistinctEntries($season1);
-        $distinctEntryCountSeason2 = $this->countDistinctEntries($season2);
+        try {
+            $distinctEntryCountSeason2 = $this->countDistinctEntries($season2);
+        } catch(\Exception $e) {
+            return $distinctEntryCountSeason1[0]['distinctEntryCount'];
+        }
+
 
         return $distinctEntryCountSeason1[0]['distinctEntryCount'] - $distinctEntryCountSeason2[0]['distinctEntryCount'];
     }
@@ -292,7 +306,7 @@ class EntryReportQuery
         try {
             $averageSeason2 = $this->calculateAverageEntriesPerPool($season2);
         } catch(\Exception $e) {
-            $averageSeason2 = 0;
+            return $averageSeason1;
         }
 
 
