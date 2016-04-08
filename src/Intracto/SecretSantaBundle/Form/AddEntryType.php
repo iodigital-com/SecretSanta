6f37ea96f88e39862cb76eda5d9a66c481563ee7
NIELS ROELS <niels.roels@student.thomasmore.be>
2016-04-08 16:37:33 +0200

    Created form to add new entry to pool

---
+++ src/Intracto/SecretSantaBundle/Form/AddEntryType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Intracto\SecretSantaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AddEntryType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => 'Intracto\SecretSantaBundle\Entity\Entry',
+            ]
+        );
+
+    }
+
+    public function getName()
+    {
+        return 'intracto_secretsantabundle_addentrytype';
+    }
+}
