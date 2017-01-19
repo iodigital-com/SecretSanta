ffa96f126e2a5799d843cd7ab64ceb9fa818efdf
Wouter Van Scharen <wouter.vanscharen@intracto.com>
2017-01-19 19:07:07 +0100

    156: Stop mailing wishlists overview and display as page

--- src/Intracto/SecretSantaBundle/Controller/Wishlist/ExposeWishlistsController.php
+++ src/Intracto/SecretSantaBundle/Controller/Wishlist/ExposeWishlistsController.php
@@ -2,6 +2,7 @@
 
 namespace Intracto\SecretSantaBundle\Controller\Wishlist;
 
+use Intracto\SecretSantaBundle\Entity\Pool;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -11,42 +12,24 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class ExposeWishlistsController extends Controller
 {
     /**
-     * @Route("/expose_wishlists/{listUrl}", name="pool_expose_wishlists")
-     * @Template()
+     * @Route("/wishlists/expose/{listUrl}", name="pool_expose_wishlists")
+     * @Template("IntractoSecretSantaBundle:Wishlist:expose.html.twig")
      */
     public function exposeWishlistsAction(Request $request, $listUrl)
     {
-        $correctCsrfToken = $this->isCsrfTokenValid(
-            'expose_wishlists',
-            $request->get('csrf_token')
-        );
 
-        $correctConfirmation = (strtolower($request->get('confirmation')) === strtolower($this->get('translator')->trans('expose_wishlists.phrase_to_type')));
-
-        if ($correctConfirmation === false || $correctCsrfToken === false) {
-            $this->get('session')->getFlashBag()->add(
-                'danger',
-                $this->get('translator')->trans('flashes.expose_wishlists.not_exposed')
-            );
-
-            return $this->redirect($this->generateUrl('pool_manage', ['listUrl' => $listUrl]));
-        }
-
+        /** @var Pool $pool */
         $pool = $this->get('pool_repository')->findOneByListurl($listUrl);
         if (!is_object($pool)) {
             throw new NotFoundHttpException();
         }
+
         $pool->exposeWishlists();
 
         $this->get('doctrine.orm.entity_manager')->flush();
 
-        $this->get('intracto_secret_santa.mail')->sendAllWishlistsToAdmin($pool);
-
-        $this->get('session')->getFlashBag()->add(
-            'success',
-            $this->get('translator')->trans('flashes.expose_wishlists.exposed')
-        );
-
-        return $this->redirect($this->generateUrl('pool_manage', ['listUrl' => $listUrl]));
+        return [
+            'pool' => $pool,
+        ];
     }
 }
