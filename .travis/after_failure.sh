eb809840f6078818042af70dee3e1249ec29837f
Jeroen Thora <jeroenthora@gmail.com>
2017-02-26 20:22:53 +0100

    Uploading behat screenshot when testsuite fails

--- .travis/after_failure.sh
+++ .travis/after_failure.sh
@@ -1,3 +1,18 @@
 #!/usr/bin/env bash
 
 bin/upload-textfiles "${TRAVIS_BUILD_DIR}/*.log"
+
+echo "Uploading behat failed step screenshots..."
+
+# Please generate a client key for yourself and don't use ours to avoid reaching the rate limit.
+clientid='212729d137b2466'
+
+for image in ${TRAVIS_BUILD_DIR}/*.png
+do
+    res=$(curl -sH "Authorization: Client-ID $clientid" -F "image=@$image" "https://api.imgur.com/3/upload")
+
+    echo $res | grep -qo '"status":200' && link=$(echo $res | sed -e 's/.*"link":"\([^"]*\).*/\1/' -e 's/\\//g')
+
+    #Print uploaded image url
+    echo $link
+done
