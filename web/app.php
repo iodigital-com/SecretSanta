4a83661b0579fbbcb48b93bf0a377f0e1c626218
Tom Van Looy <tom@ctors.net>
2017-03-07 14:34:03 +0100

    Remove loadClassCache

--- web/app.php
+++ web/app.php
@@ -9,7 +9,6 @@ $loader = require __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
 
 $kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
--- web/app_dev.php
+++ web/app_dev.php
@@ -17,7 +17,6 @@ die($_SERVER['REMOTE_ADDR']);
 $loader = require __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
 $kernel = new AppKernel('dev', true);
-$kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
