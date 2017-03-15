b148a4a07de18ac9f792859adfcdb75ec1b0ba20
Tom Van Looy <tom@ctors.net>
2017-03-15 18:45:59 +0100

    #215: Unused

--- src/Intracto/SecretSantaBundle/Controller/Pool/ManagementController.php
+++ src/Intracto/SecretSantaBundle/Controller/Pool/ManagementController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Intracto\SecretSantaBundle\Form\AddEntryType;
-use Intracto\SecretSantaBundle\Entity\Pool;
 use Intracto\SecretSantaBundle\Entity\Entry;
 use Intracto\SecretSantaBundle\Form\UpdatePoolDetailsType;
 
