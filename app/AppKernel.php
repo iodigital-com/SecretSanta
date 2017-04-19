7b70816495f5dc2aa3814be646a9719ca3823fdb
Mats Vanspringel <mats@vanspringel.net>
2017-04-19 09:42:33 +0200

    Added bugsnag to project Added bugsnag, to get extra information about current bugs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Intracto\SecretSantaBundle\IntractoSecretSantaBundle(),
             new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new \Bugsnag\BugsnagBundle\BugsnagBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test', 'test_travis'))) {
