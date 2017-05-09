450930573532fac729cfc023caf477f91da03a01
Tom Van Looy <tom@ctors.net>
2017-05-09 14:05:39 +0200

    #287: Add rsyslog-mysql

--- shell_provisioner/module/rsyslogd.sh
+++ shell_provisioner/module/rsyslogd.sh
@@ -2,5 +2,9 @@
 
 # rsyslogd
 
+echo "rsyslog-mysql rsyslog-mysql/dbconfig-install boolean true" | debconf-set-selections
+apt-get -y install rsyslog-mysql
+
 cat $CONFIG_PATH/rsyslogd/postfix.conf > /etc/rsyslog.d/postfix.conf
 service rsyslog restart
+
