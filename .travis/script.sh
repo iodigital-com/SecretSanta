0540de144ba86f4e09fb1490413377a3102b0d14
Jeroen Thora <jeroenthora@gmail.com>
2017-07-24 15:14:40 +0200

    Use ubuntu trusty and upgrade selenium

--- .travis/script.sh
+++ .travis/script.sh
@@ -10,15 +10,15 @@ chromium-browser --version
 
 # Download and configure ChromeDriver
 if [ ! -f $BUILD_CACHE_DIR/chromedriver ]; then
-    # Using ChromeDriver 2.12 which supports Chrome 36-40, we're using Chromium 37
-    curl http://chromedriver.storage.googleapis.com/2.12/chromedriver_linux64.zip > chromedriver.zip
+    # Using ChromeDriver 2.30 which supports Chrome 58-60, we're using Chromium 59
+    curl http://chromedriver.storage.googleapis.com/2.30/chromedriver_linux64.zip > chromedriver.zip
     unzip chromedriver.zip
     mv chromedriver $BUILD_CACHE_DIR
 fi
 
 # Run Selenium with ChromeDriver
 echo "Start selenium"
-bin/selenium-server-standalone -Dwebdriver.chrome.driver=$BUILD_CACHE_DIR/chromedriver > $TRAVIS_BUILD_DIR/selenium.log 2>&1 &
+PATH=\"$BUILD_CACHE_DIR:\$PATH\" bin/selenium-server-standalone > $TRAVIS_BUILD_DIR/selenium.log 2>&1 &
 
 # Run phpunit tests (Temporary re-enable xdebug to generate coverage report)
 phpenv config-add ~/xdebug.ini
