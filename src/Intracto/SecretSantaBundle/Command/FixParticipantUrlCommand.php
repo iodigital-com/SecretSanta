2d6375ec060f37774804c80c8cd56e7b0b582036
Tom Van Looy <tom@ctors.net>
2017-11-23 18:31:01 +0100

    Fix

---
+++ src/Intracto/SecretSantaBundle/Command/FixParticipantUrlCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Intracto\SecretSantaBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Intracto\SecretSantaBundle\Entity\ParticipantRepository;
+use GeoIp2\Database\Reader;
+use GeoIp2\Exception\AddressNotFoundException;
+
+class FixParticipantUrlCommand extends Command
+{
+    /** @var EntityManagerInterface */
+    private $em;
+
+    public function __construct(
+        EntityManagerInterface $em
+    ) {
+        $this->em = $em;
+
+        parent::__construct();
+    }
+
+    protected function configure()
+    {
+        $this->setName('intracto:fix:participant_info');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $participants = $this->participantRepository->findBy(['url' => null]);
+        foreach ($participants as $participant) {
+            if (!empty($participant->getUrl())) {
+                echo 'This should not happen!';
+                die();
+            }
+            $participant->setUrl(base_convert(sha1(uniqid(mt_rand(), true)), 16, 36));
+
+            $this->em->persist($participant);
+            $this->em->flush();
+        }
+
+    }
+}
