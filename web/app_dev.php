4e6dd77da9e5b6e8f4602efe06637052ea8629a3
Tom Van Looy <tom@ctors.net>
2017-12-12 23:03:29 +0100

    Update app_dev.php
    document the development IP's

--- web/app_dev.php
+++ web/app_dev.php
@@ -7,8 +7,8 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
     || !in_array(@$_SERVER['REMOTE_ADDR'], array(
         '127.0.0.1',
         '::1',
-        '192.168.33.1',
-        '172.18.0.1',
+        '192.168.33.1', // for Vagrant
+        '172.18.0.1',   // for Docker
     ))
 ) {
 die($_SERVER['REMOTE_ADDR']);
