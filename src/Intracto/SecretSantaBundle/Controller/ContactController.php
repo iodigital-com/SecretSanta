06e87440dd3e544988f2fc42903d6be9866ce877
Mats Vanspringel <mats.vanspringel@intracto.com>
2018-11-09 16:36:01 +0100

    Added contact form

---
+++ src/Intracto/SecretSantaBundle/Controller/ContactController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Intracto\SecretSantaBundle\Controller;
+
+use Intracto\SecretSantaBundle\Form\Handler\ContactFormHandler;
+use Intracto\SecretSantaBundle\Form\Type\ContactType;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * @Route("/contact", name="contact")
+     * @Template("IntractoSecretSantaBundle:Static:contact.html.twig")
+     * @Method({"GET", "POST"})
+     *
+     * @param Request $request
+     * @param ContactFormHandler $handler
+     */
+    public function indexAction(Request $request, ContactFormHandler $handler)
+    {
+        $form = $this->createForm(ContactType::class);
+
+        if ($handler->handle($form, $request)) {
+            return $this->redirectToRoute('homepage');
+        }
+
+        return ['form' => $form->createView()];
+    }
+}
