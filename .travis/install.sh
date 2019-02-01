96f9ddaf30b46457568ffba655e516e3a82db56c
Tom Van Looy <tom@ctors.net>
2019-02-01 15:00:33 +0100

    Provide clients_secrets for tests

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
 echo "Install dependencies"
-
 composer install --no-interaction --no-scripts --prefer-dist --classmap-authoritative || exit $?
 
 echo "Warming up dependencies"
 composer run-script travis-build --no-interaction || exit $?
 
 echo "building frontend"
+cat << EOF >app/config/client_secrets.json
+{
+    "key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "secret_key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+    "action": "contact",
+    "threshold": 0.5
+}
+EOF
 yarn
 yarn build
