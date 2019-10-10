b25eea64e0bbddca8153307de3024cf366ecb8ca
Tom Van Looy <tom@ctors.net>
2019-10-10 23:08:00 +0200

    provisioning fixes

--- shell_provisioner/module/yarn.sh
+++ shell_provisioner/module/yarn.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-# Nodejs
+# Frontend modules
 
+# Install Yarn first
+wget -qO - https://dl.yarnpkg.com/debian/pubkey.gpg | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+apt-get update
+apt-get install -y yarn
+
+# Install Node.js second as dependency for Yarn
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
 apt-get install -y nodejs
 
-# Yarn
-
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-
-apt-get update
-apt-get install -y yarn
+# Install module dependencies
+apt-get install -y gcc g++ make
