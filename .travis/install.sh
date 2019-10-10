251230ca00d48c06ab167b11c7d9f4c330a5a7ed
Tom Van Looy <tom@ctors.net>
2019-10-10 10:47:46 +0200

    No disable platform-reqs

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Install dependencies"
-composer install --ignore-platform-reqs --no-interaction --no-scripts --prefer-dist --classmap-authoritative || exit $?
+composer install --no-interaction --no-scripts --prefer-dist --classmap-authoritative || exit $?
 
 echo "Warming up dependencies"
 composer run-script travis-build --no-interaction || exit $?
