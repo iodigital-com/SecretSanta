01d60853f997c4e8d9b209ea33dcbe40623055e9
Tom Van Looy <tom@ctors.net>
2020-01-06 08:38:10 +0100

    Unmaintained

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,7 +10,6 @@ if (isset($_SERVER['HTTP_CLIENT_IP'])
         '127.0.0.1',
         '::1',
         '192.168.33.1', // for Vagrant
-        '172.18.0.1',   // for Docker
     ))
 ) {
     die($_SERVER['REMOTE_ADDR']);
