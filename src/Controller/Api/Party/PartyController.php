de0f507fbdc7fcd906c6ea3bd525b667451d82bb
Tom Cannaerts <tom.cannaerts@intracto.com>
2021-09-03 20:35:49 +0200

    Combine 2 Controllers into 1

--- src/Controller/Api/Party/PartyController.php
+++ src/Controller/Api/Party/PartyController.php
@@ -5,19 +5,37 @@ declare(strict_types=1);
 namespace App\Controller\Api\Party;
 
 use App\Controller\Api\ApiHelper;
+use App\Entity\Party;
 use App\Service\PartyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use App\Entity\Party;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class PartyController extends AbstractController
 {
+
+    public function __construct(
+        private NormalizerInterface $normalizer
+    ) {}
+
+    /**
+     * @Route("/api/party/{listurl}", name="api_party_get", methods={"GET"})
+     * @return Response
+     */
+    public function getParty(Party $party): Response
+    {
+        return new JsonResponse(
+            $this->normalizer->normalize($party)
+        );
+    }
+
     /**
      * @Route("/api/party", name="api_party_create", methods={"POST"})
      */
-    public function createAction(Request $request, PartyService $partyService)
+    public function createAction(Request $request, PartyService $partyService): Response
     {
         // get Json object. If JsonResponse is returned
         $object = ApiHelper::getRequestBody($request);
