694bc19bb61a72d204c5cdc1ca5c39798a236541
Tom Van Looy <tom@ctors.net>
2023-08-18 23:22:01 +0200

    Remove Travis

--- .travis/before_script.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Setup application"
-bin/console doctrine:database:create --env=test -vvv || exit $?
-bin/console cache:warmup --env=test --no-debug -vvv || exit $?
-bin/console doctrine:schema:update --force --env=test || exit $?
-
-echo "Setting the web assets up"
-bin/console assets:install --env=test --no-debug -vvv || exit $?
