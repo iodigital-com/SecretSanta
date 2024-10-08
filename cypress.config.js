ed132507951dddf3332586f8214017ceefc6d895
Chuck Chiu <chuck.chiu@iodigital.com>
2024-10-08 10:25:53 +0200

    SANTA-222: increase Cypress default timeout

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'https://secretsanta.lndo.site',
     setupNodeEvents(on, config) {
